776a3173481df81604450d81c099363464209b5e
Danya <daniildocenko218@gmail.com>
2023-02-04 19:07:22 +0200

    add variables

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        double firstNumber = scanner.nextDouble();
+        System.out.println("Введите число");
+        double secondNumber = scanner.nextDouble();
+        System.out.println("Введите знак");
+        String sign = scanner.next();
 
     }
 }
